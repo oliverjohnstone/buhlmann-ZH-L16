e129fb8ffcae8573ca4a4d06e35a550658739b74
Oliver Johnstone <ojohnstone@cegid.com>
2023-02-11 13:18:37 +0000

    Fix lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,19 @@ module.exports = {
     es2021: true
   },
   extends: 'standard-with-typescript',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking'
+      ],
+      parserOptions: {
+        project: ['./tsconfig.json']
+      }
+    }
   ],
   parserOptions: {
     ecmaVersion: 'latest',
